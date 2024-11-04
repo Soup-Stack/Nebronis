1373a4973f8452870f461e458e24e959f0243223
supriya <hm7020@gmail.com>
2024-11-04 16:21:02 +0100

    this is just a test really

---
+++ Nebronis/TEST.swift
@@ -0,0 +1,19 @@
+//
+//  TEST.swift
+//  Nebronis
+//
+//  Created by Palle Supriya on 04/11/24.
+//
+
+import SwiftUI
+
+struct TESTView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello")
+        }
+    }
+}
