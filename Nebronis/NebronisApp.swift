ce8b09bb0cdb718c8852854e25f668af47bece68
supriya <hm7020@gmail.com>
2024-11-04 15:35:07 +0100

    Initial Commit

---
+++ Nebronis/NebronisApp.swift
@@ -0,0 +1,17 @@
+//
+//  NebronisApp.swift
+//  Nebronis
+//
+//  Created by Palle Supriya on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NebronisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
